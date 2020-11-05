73cdbcc2e4a5758662d2c5f7012d0e4e42cf4845
memo162 <jgespinosa91@gmail.com>
2020-11-05 09:37:03 -0500

    HOW-16: adding home navigation buttons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './components/home/home.component';
 import { RouterModule, Routes } from '@angular/router';
+import { CharactersComponent } from './components/characters/characters.component';
+import { StudentsComponent } from './components/students/students.component';
+import { TeachersComponent } from './components/teachers/teachers.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'characters', component: CharactersComponent },
+  { path: 'students', component: StudentsComponent },
+  { path: 'teachers', component: TeachersComponent }
 ];
 
 @NgModule({
